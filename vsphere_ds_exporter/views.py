cb462f09f7e7aebcb6729cf83d0a5945b0926b3e
xujpxm <329771774@qq.com>
2017-09-20 18:34:32 +0800

    exporter daemon

--- vsphere_ds_exporter/views.py
+++ vsphere_ds_exporter/views.py
@@ -3,6 +3,7 @@
 
 from __future__ import absolute_import, unicode_literals
 from flask import Blueprint, render_template
+from flask import Response
 
 metrics = Blueprint('metrics', __name__)
 
@@ -14,4 +15,10 @@ def index():
 
 @metrics.route('/metrics')
 def details():
-    return render_template('metrics.html')
+    str = """# HELP vsphere_datastore_capacity datastore capacity
+# TYPE vsphere_datastore_capacity gauge
+vsphere_datastore_capacity_bytes{name="NYFCLOUD-TYPE03-VOL"} 1145414090752
+vsphere_datastore_freespace_bytes{name="NYFCLOUD-TYPE03-VOL"} 150457024512
+vsphere_datastore_uncommitted_bytes{name="NYFCLOUD-TYPE03-VOL"} 5498350192533
+    """
+    return Response(str, mimetype='text/plain')
